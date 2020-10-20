1e73ef3833e077e234867679e67914599d63d4cf
JustinRush80 <69156844+JustinRush80@users.noreply.github.com>
2020-10-20 09:59:23 -0400

    Update optimization_setup.py

--- test/Himmelblau/serial/optimization_setup.py
+++ test/Himmelblau/serial/optimization_setup.py
@@ -33,5 +33,5 @@ sode.add_performance_parameters(performance_params = performance_parameters)
 sode.mutation_params.mutation_type = de_mutation_type.de_1_rand_bin
 sode.mutation_params.min_parents = int(0.1*pop_size)
 sode.mutation_params.max_parents = int(0.9*pop_size)
-sode.start_doe(doe_size=32)
+sode.start_doe(doe_size=16)
 sode.optimize_from_population(pop_start=-1,n_generations=15)
