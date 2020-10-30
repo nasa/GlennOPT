15bb5998e808de6bf5abac278ae5cde024a7f60a
Paht J <paht.juangphanich@gmail.com>
2020-10-30 16:59:01 -0400

    small fixes to multi-objective probe

--- test/ProbePlacement_multi/parallel/optimization_plot.py
+++ test/ProbePlacement_multi/parallel/optimization_plot.py
@@ -2,8 +2,46 @@
 
 import sys,os
 sys.path.insert(0,'../../../')
-from glennopt.helpers import Parameter, parallel_settings
-from glennopt.nsga3 import NSGA3,mutation_parameters, de_mutation_type
+from glennopt.base import Parameter
+from glennopt.helpers import mutation_parameters, de_mutation_type
+from glennopt.sode import SODE, selection_type
+from glennopt.nsga3 import NSGA3
+from glennopt.DOE import Default,CCD,FullFactorial,LatinHyperCube
+import numpy as np
+import os
+
+
+# Initialize the DOE 
+doe = LatinHyperCube(samples=128,levels=4) # 128 random samples of the design space
+# These are also available for use
+# doe = FullFactorial(levels=2) 
+# doe = Default(15) # Default
+# doe = CCD()
+
+
+# Define the Evaluation Parameters 
+# Define evaluation parameters 
+nProbes = 10
+minSpacing = 3
+probeSpacing = 360/nProbes
+tLo     = np.zeros(nProbes)
+tHi     = np.zeros(nProbes)
+for i in range(nProbes):
+    tLo[i] = probeSpacing*i
+    if i != nProbes-1:
+        tHi[i] = probeSpacing*(i+1) - minSpacing
+    else:
+        tHi[-1] = probeSpacing*(i+1)    
+    doe.add_parameter(name="x"+str(i+1),min_value=tLo[i],max_value=tHi[i])
+constraints = (tLo,tHi)
+
+doe = LatinHyperCube(samples=128,levels=5)
+doe.add_objectives(name='objective1')
+doe.add_objectives(name='objective2')
+
+# Define any performance parameters you want to keep track of (tracking only)
+doe.add_perf_parameter(name='PearsonR')
+doe.add_perf_parameter(name='RMS_Error')
 
 # Generate the DOE
 pop_size=48
